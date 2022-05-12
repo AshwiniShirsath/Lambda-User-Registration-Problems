03b51b6da1a662afccf812e9492655298be29fd2
AshwiniShirsath <shirsathashwini9495@gmail.com>
2022-05-12 08:42:20 +0530

    [ADDED] : Modified User Registration Using LAMBDA Expression

---
+++ src/com/bridgelabz/lambdauserregistration/UserRegistrationRegex.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.lambdauserregistration;
+
+public class UserRegistrationRegex {
+    public static void main(String[] args) throws Exception {
+        UserRegistration userInformation = new UserRegistration();
+
+        System.out.println(userInformation.userFirstName());
+        System.out.println(userInformation.userLastName());
+        System.out.println(userInformation.userEmailId());
+        System.out.println(userInformation.userMobNum());
+        System.out.println(userInformation.userPassword());
+    }
+}
---
+++ src/com/bridgelabz/lambdauserregistration/ValidationInterface.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.lambdauserregistration;
+
+public interface ValidationInterface {
+    public String validate(String str);
+}
